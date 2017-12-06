a1521684422d0546e71f429859c823f07597f9e1
Dominik Schmidtlein <dominik.schmidtlein@gmail.com>
2017-12-05 22:43:03 -0500

    calculated number of tweets given search

--- routes/index.js
+++ routes/index.js
@@ -2,25 +2,36 @@ var express = require('express');
 var router = express.Router();
 var Twitter = require('twitter');
 
+var client = new Twitter({
+  consumer_key: process.env.TWITTER_CONSUMER_KEY,
+  consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
+  bearer_token: process.env.TWITTER_BEARER_TOKEN
+});
+
+var count = 100;
+var tweetCount = 0;
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  var client = new Twitter({
-    consumer_key: process.env.TWITTER_CONSUMER_KEY,
-    consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
-    bearer_token: process.env.TWITTER_BEARER_TOKEN
-  });
-
-  var search = `#${req.query.search}`;
-
-  client.get('search/tweets', { q: search }, function(error, tweet, response) {
-    if (error) throw error;
-    console.log(Object.keys(tweet.search_metadata));
-    console.log(tweet.search_metadata.count);
-    // var tweets = tweet.statuses.map(x => x.text);
-    // console.log(tweets);
-  });
-
-  res.render('index', { title: 'Express' });
+  var q = `${req.query.search}`;
+  getTweets('search/tweets', { q, count, result_type: 'popular' }, () => displayTweets(res, req.query.search));
 });
+
+function getTweets(url, params, callback) {
+  client.get(url, params, function(error, tweet, response) {
+    if (error) throw error;
+    tweetCount += tweet.statuses.length;
+    if (tweet.search_metadata.next_results) { // more tweets available
+      params.max_id = tweet.search_metadata.next_results.match(/max_id=(\d{18})&/)[1];
+      getTweets(url, params, callback);
+    } else { // no more tweets
+      callback();
+    }
+  });
+}
+
+function displayTweets(res, search) {
+  res.render('index', { title: 'Express', search, count: tweetCount });
+}
 
 module.exports = router;
