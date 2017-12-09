a3ec29b49315ed851cc49111ef51e7e98e85418c
Dominik Schmidtlein <dominik.schmidtlein@gmail.com>
2017-12-08 21:25:11 -0500

    getting twitter handle and tweet count from user company search

--- routes/index.js
+++ routes/index.js
@@ -1,37 +1,66 @@
 var express = require('express');
 var router = express.Router();
 var Twitter = require('twitter');
+var request = require('request');
+var url = require('url');
 
 var client = new Twitter({
   consumer_key: process.env.TWITTER_CONSUMER_KEY,
   consumer_secret: process.env.TWITTER_CONSUMER_SECRET,
-  bearer_token: process.env.TWITTER_BEARER_TOKEN
+  // bearer_token: process.env.TWITTER_BEARER_TOKEN
+  access_token_key: process.env.TWITTER_ACCESS_TOKEN_KEY,
+  access_token_secret: process.env.TWITTER_ACCESS_TOKEN_SECRET
 });
-
-var count = 100;
-var tweetCount = 0;
 
 /* GET home page. */
+
 router.get('/', function(req, res, next) {
-  var q = `${req.query.search}`;
-  getTweets('search/tweets', { q, count, result_type: 'popular' }, () => displayTweets(res, req.query.search));
+  var search = `${req.query.search}`;
+
+  // search company stock symbol
+  request({
+    url: 'http://d.yimg.com/autoc.finance.yahoo.com/autoc',
+    qs: { query: search, region: 1, lang: 'en' }
+  }, function(error, response, body) {
+    if (error) throw error;
+    var symbol = JSON.parse(body).ResultSet.Result[0].symbol;
+    let q = search + " filter:verified";
+
+    // search user by input query
+    client.get('users/search', { q, count: 5 }, function(error, body, response) {
+      if (error) throw error;
+      let t_account = body.reduce((a, b) => a.followers_count > b.followers_count ? a : b);
+      var company = t_account.name;
+      var handle = t_account.screen_name;
+      let q = `from:${handle}`;
+
+      // search tweets by input company's handle
+      searchTweets('search/tweets', { q, count: 100 }, function(tweets) {
+        res.render('index', {
+          search,
+          symbol,
+          company,
+          handle,
+          count: tweets.length
+        });
+      });
+    });
+  });
 });
 
-function getTweets(url, params, callback) {
-  client.get(url, params, function(error, tweet, response) {
+// get all the pages of tweets
+function searchTweets(urlString, params, callback, tweets = []) {
+  client.get(urlString, params, function(error, body, response) {
     if (error) throw error;
-    tweetCount += tweet.statuses.length;
-    if (tweet.search_metadata.next_results) { // more tweets available
-      params.max_id = tweet.search_metadata.next_results.match(/max_id=(\d{18})&/)[1];
-      getTweets(url, params, callback);
+    tweets.push(...body.statuses);
+    let next = body.search_metadata.next_results;
+    if (next) { // more tweets available
+      params.max_id = url.parse(next, true).query.max_id;
+      searchTweets(urlString, params, callback, tweets);
     } else { // no more tweets
-      callback();
+      callback(tweets);
     }
   });
-}
-
-function displayTweets(res, search) {
-  res.render('index', { title: 'Express', search, count: tweetCount });
 }
 
 module.exports = router;
